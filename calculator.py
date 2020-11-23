d4626d58833b2b5777b2a5fcc80a8ca515d9e662
Luis Villén <l.villenjaldon@gmail.com>
2020-11-24 00:34:14 +0100

    Version 0.0

--- calculator.py
+++ calculator.py
@@ -1,1 +1,42 @@
-#TO DO
+from tkinter import *
+from tkinter import ttk
+
+WIDTH = 68
+HEIGHT = 50
+
+class Display(ttk.Frame):
+
+    def __init__(self, parent):
+        #Defino el cuadro sobre el que luego pintaré el objeto
+        ttk.Frame.__init__(self, parent, width=WIDTH*4, height=HEIGHT)
+        self.pack_propagate(0)
+
+        #Defino el estilo para poder trabajar 'bien' porque 'aqua' no nos deja.
+        s = ttk.Style()
+        s.theme_use('alt')
+
+        #Defino la 'label'
+        self.label = ttk.Label(self, text='0', anchor=E, background='black', foreground='white', font='Helvetica 36')
+        #Empaquetado
+        self.label.pack(side=TOP, fill=BOTH, expand=True)
+
+class CalcButton(ttk.Frame):
+
+    def __init__(self, parent, text, command=None, width=1, height=1):
+        #Defino el cuadro sobre el que luego pintaré el objeto
+        ttk.Frame.__init__(self, parent, width=WIDTH*width, height=HEIGHT*height)
+        self.pack_propagate(0) #Para que mi tamaño mande
+        
+        #Defino el estilo para poder trabajar
+        s = ttk.Style()
+        s.theme_use('alt')
+
+        #Como no voy a tener que recuperar los valores del botón, puedo hacerlo todo en una misma línea y sin definir variable
+        ttk.Button(self, text=text, command=command).pack(side=TOP, fill=BOTH, expand=True)
+
+        #Defino el botón
+        #btn = ttk.Button(self, text=text, command=command)
+        #Empaquetado
+        #btn.pack(side=TOP, fill=BOTH, expand=True)
+
+
--- main.py
+++ main.py
@@ -1,4 +1,57 @@
 from tkinter import *
 from tkinter import ttk
+import calculator
+
+def imprimeuno():
+    print(1)
 
 class MainApp(Tk):
+    
+    def __init__(self):
+        Tk.__init__(self)
+        #self.geometry('272x300') No hace falta porque ya lo hemos definido con el tamaño de self.display y self.teclado
+        self.title('Calculadora')
+
+        self.display = calculator.Display(self)
+        self.display.pack(side=TOP)
+
+        self.teclado = ttk.Frame(self, width=calculator.WIDTH*4, height=calculator.HEIGHT*5)
+        self.teclado.grid_propagate(0)
+        self.teclado.pack(side=TOP, fill=BOTH, expand=True)
+
+        botonC = calculator.CalcButton(self.teclado, 'C')
+        botonC.grid(column=0, row=0)
+
+        botonC = calculator.CalcButton(self.teclado, '+/-')
+        botonC.grid(column=1, row=0)
+
+#TERMINAR DE MONTAR LA CALCULADORA
+#PENSAR QUÉ BUCLE PODEMOS MONTAR, INFORMANDO DE CADA BOTÓN (Diccionario, lista...)
+
+        '''
+        self.calcButtonC = ttk.Frame(self, width=136, height=50)
+        btn = ttk.Button(self.calcButtonC, text='C')
+        self.calcButtonC.pack_propagate(False)
+        btn.pack(side=TOP, fill=BOTH, expand=True)
+        self.calcButtonC.grid(column=0, row=1, columnspan=2)
+
+        self.calcButtonCs = ttk.Frame(self, width=68, height=50)
+        btn = ttk.Button(self.calcButtonCs, text='+/-')
+        self.calcButtonCs.pack_propagate(False)
+        btn.pack(side=TOP, fill=BOTH, expand=True)
+        self.calcButtonCs.grid(column=2, row=1)
+
+        self.calcButtondiv = ttk.Frame(self, width=68, height=50)
+        btn = ttk.Button(self.calcButtondiv, text='÷')
+        self.calcButtondiv.pack_propagate(False)
+        btn.pack(side=TOP, fill=BOTH, expand=True)
+        self.calcButtondiv.grid(column=3, row=1)
+
+        #self.mainloop() Esto tiene que ir siempre al final
+        '''
+
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.mainloop()
